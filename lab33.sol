5f91bb756cb09da332a00852ad4ce970774279e7
vankhoa21991 <vankhoa21991@gmail.com>
2021-09-19 20:00:24 +0200

    lab33 add owner acc

--- lab33.sol
+++ lab33.sol
@@ -2,10 +2,14 @@ pragma solidity ^0.5.13;
 
 contract StartStopUpdateExample {
     function sendMoney() public payable {
+        address owner;
 
+        constructor() public {
+            owner = msg.sender;
+        }
     }
 
     function withdrawAllMonay( address payable _to) public {
-        _to.transfer(address(this).balance);
+        _to.transfer(address(this).balance);    
     }
 }
