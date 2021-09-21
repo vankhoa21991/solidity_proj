3483ffaec10c1d96adab52a131cf1a1e353e7f20
vankhoa21991 <vankhoa21991@gmail.com>
2021-09-21 23:10:57 +0200

    update lab39 and add lab39

--- lab38.sol
+++ lab38.sol
@@ -1,15 +1,27 @@
 pragma solidity ^0.5.13;
 
 contract MappingsStructExample {
+    
+    mapping (address => uint) public balanceReceived;
+    
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 
     function sendMoney() public payable {
+        balanceReceived[msg.sender] += msg.value;
 
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(balanceReceived[msg.sender] >= _amount, "not enough funds");
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
     }
 
     function withdrawAllMoney(address payable _to) public {
-        _to.transfer(address(this).balance);
+        uint balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender] = 0;
+        _to.transfer(balanceToSend);
     }
 }
