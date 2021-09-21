60693fa12fec4fdcf454adbf99c5423584ac56c7
vankhoa21991 <vankhoa21991@gmail.com>
2021-09-21 22:23:48 +0200

    add lab38

---
+++ lab38.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.13;
+
+contract MappingsStructExample {
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function sendMoney() public payable {
+
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        _to.transfer(address(this).balance);
+    }
+}
