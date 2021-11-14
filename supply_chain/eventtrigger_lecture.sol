a68f010c4d3679bda23038c6bf1e4d7034f2017d
vankhoa21991 <vankhoa21991@gmail.com>
2021-11-14 22:53:26 +0100

    add item contract

--- supply_chain/eventtrigger_lecture.sol
+++ supply_chain/eventtrigger_lecture.sol
@@ -1,10 +1,36 @@
 pragma solidity ^0.6.0;
 
+
+contract Item {
+    uint public priceInWei;
+    uint public index;
+    uint public pricePaid;
+    
+    ItemManager parentContract;
+    
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
+        priceInWei = _priceInWei;
+        index = _index;
+        parentContract = _parentContract;
+    }
+    
+    receive() external payable {
+        require(pricePaid == 0, "Item is paid already");
+        require(priceInWei == msg.value, "Only full payments allowed");
+        pricePaid += msg.value;
+        (bool success, ) = address(parentContract).call.value(msg.value)(abi.encodeWithSignature("triggerPayment(uint256)", index));
+    
+        require(success, "The transaction wasn't successful, canceling");
+    }
+} 
+    
+
 contract ItemManager{
     
     enum SupplyChainState{Created, Paid, Delivered}
     
     struct S_Item{
+        Item _item;
         string _identifier;
         uint _itemPrice;
         ItemManager.SupplyChainState _state;
@@ -16,6 +42,8 @@ contract ItemManager{
     
     function creatItem( string memory _identifier, uint _itemPrice) public {
         items[itemIndex]._identifier = _identifier;
+        Item item = new Item(this, _itemPrice, itemIndex);
+        items[itemIndex]._item = item
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
         
